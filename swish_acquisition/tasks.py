29b5720303e3a7e5b73f3b7e385f2c656abc3cb4
usharerose <ushareroses@gmail.com>
2024-02-16 15:07:29 +0800

    refactor: integrate Minio storage into task

--- swish_acquisition/tasks.py
+++ swish_acquisition/tasks.py
@@ -5,7 +5,7 @@ import datetime
 import logging
 
 from swish_acquisition.celery_app import app
-from swish_acquisition.scoreboardv3 import ScoreboardV3Endpoint
+from swish_acquisition.collectors import ScoreboardCollector
 
 
 logger = logging.getLogger(__name__)
@@ -14,5 +14,5 @@ logger = logging.getLogger(__name__)
 @app.task
 def scrape_daily_scoreboard(game_date: str, league_id: str):
     a_date = datetime.datetime.strptime(game_date, '%Y-%m-%d').date()
-    endpoint = ScoreboardV3Endpoint(game_date=a_date, league_id=league_id)
-    endpoint.get_data()
+    collector = ScoreboardCollector(game_date=a_date, league_id=league_id)
+    collector.run()
