b6dee2f3188132454928a9619cd97ca76085cc53
usharerose <ushareroses@gmail.com>
2024-02-17 22:09:47 +0800

    feat: implement BoxScoreSummary collector

--- swish_acquisition/collectors/__init__.py
+++ swish_acquisition/collectors/__init__.py
@@ -1,4 +1,5 @@
 """
 NBA raw statistics collector from remote to S3
 """
+from swish_acquisition.collectors.boxscoresummary import BoxscoreSummaryCollector  # NOQA
 from swish_acquisition.collectors.scoreboard import ScoreboardCollector  # NOQA
---
+++ swish_acquisition/collectors/boxscoresummary.py
@@ -0,0 +1,25 @@
+"""
+Collect and store Boxscore Summary raw data
+"""
+from typing import Dict
+
+from swish_acquisition.collectors.base import EndpointCollectorMixIn
+from swish_acquisition.endpoints import BoxScoreSummaryV3Endpoint
+from swish_acquisition.s3 import S3MixIn
+
+
+class BoxscoreSummaryCollector(BoxScoreSummaryV3Endpoint, S3MixIn, EndpointCollectorMixIn):
+
+    BUCKET_NAME = 'boxscoresummary'
+    OBJECT_NAME_PATTERN = '/{year:04d}/{month:02d}/{day:02d}/{game_id}.json'
+
+    def __init__(self, *args, **kwargs):  # NOQA
+        super().__init__(*args, **kwargs)
+
+    def get_object_path_kwargs(self) -> Dict:
+        return {
+            'year': self.game_date.year,
+            'month': self.game_date.month,
+            'day': self.game_date.day,
+            'game_id': self.game_id
+        }
