91719678ac41dd447c863378b92fe837017c0d32
usharerose <ushareroses@gmail.com>
2024-02-21 23:52:58 +0800

    feat: implement single game collect task

--- swish_acquisition/endpoints/boxscoresummaryv3.py
+++ swish_acquisition/endpoints/boxscoresummaryv3.py
@@ -2,10 +2,11 @@
 Collect boxsummaryv3 endpoint data
 """
 import datetime
-from typing import Dict, Type
+from typing import Dict, Optional, Type
 
 from swish_acquisition.endpoints.base import Endpoint
 from swish_acquisition.scheme.endpoints import BoxScoreSummaryV3
+from swish_acquisition.scheme.endpoints.boxscoresummaryv3 import TeamGameOverallStats
 
 
 class BoxScoreSummaryV3Endpoint(Endpoint):
@@ -22,3 +23,36 @@ class BoxScoreSummaryV3Endpoint(Endpoint):
         return {
             'GameID': self.game_id
         }
+
+    def get_team_ids(self) -> Dict[str, Optional[int]]:
+        away_team_id = home_team_id = None
+        dm = self.get_data()
+        if dm:
+            away_team_id = dm.boxScoreSummary.awayTeamId
+            home_team_id = dm.boxScoreSummary.homeTeamId
+        return {
+            'away': away_team_id,
+            'home': home_team_id
+        }
+
+    def get_player_ids(self) -> Dict[str, Optional[list]]:
+        away_player_ids = home_player_ids = None
+        dm = self.get_data()
+        if dm:
+            away_player_ids = self._get_roster_ids_by_team(dm.boxScoreSummary.awayTeam)
+            home_player_ids = self._get_roster_ids_by_team(dm.boxScoreSummary.homeTeam)
+        return {
+            'away': away_player_ids,
+            'home': home_player_ids
+        }
+
+    @staticmethod
+    def _get_roster_ids_by_team(team_game_stats: TeamGameOverallStats):
+        player_ids = []
+        for active_player in team_game_stats.players:
+            player_ids.append(active_player.personId)
+        for inactive_player in team_game_stats.inactives:
+            player_ids.append(inactive_player.personId)
+
+        player_ids.sort()
+        return player_ids
