3ee51bcbd074d699d34cdacc3d64d6d0160a57c5
usharerose <ushareroses@gmail.com>
2024-02-21 23:02:24 +0800

    feat: config Celery worker logger

--- swish_acquisition/celery_app.py
+++ swish_acquisition/celery_app.py
@@ -2,7 +2,19 @@
 Celery application
 """
 from celery import Celery
+from celery.signals import setup_logging
+
+from swish_acquisition.logging_config import config_logging
 
 
 app = Celery(main=__name__)
 app.config_from_object('swish_acquisition.celeryconfig')
+
+
+@setup_logging.connect
+def config_loggers(*args, **kwargs):  # NOQA
+    config_logging('swish-acquisition')
+
+
+if __name__ == '__main__':
+    app.start()
