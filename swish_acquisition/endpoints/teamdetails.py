0aa4b2b4652b32adc2802f107a679a6a2e6c3dec
usharerose <ushareroses@gmail.com>
2024-04-08 19:41:12 +0800

    feat: raw data source metadata

--- swish_acquisition/endpoints/teamdetails.py
+++ swish_acquisition/endpoints/teamdetails.py
@@ -4,13 +4,14 @@ Collect teamdetails endpoint data
 import datetime
 from typing import Any, Dict
 
+from swish_acquisition.constants import TEAM_DETAILS
 from swish_acquisition.endpoints.base import Endpoint
 from swish_acquisition.scheme.endpoints import TeamDetails
 
 
 class TeamDetailsEndpoint(Endpoint[TeamDetails]):
 
-    ENDPOINT: str = 'teamdetails'
+    ENDPOINT: str = TEAM_DETAILS.endpoint_name
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super(TeamDetailsEndpoint, self).__init__(*args, **kwargs)
